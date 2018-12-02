300574b326f5df6358b1a97b8aa03daa44214c1d
EnoshTsur <enoshh12@gmail.com>
2018-12-02 20:39:41 +0200

    'last-home-work'

--- 02-12-18/last-home-work/with_str.py
+++ 02-12-18/last-home-work/with_str.py
@@ -1,48 +1,50 @@
-# p1, p2 = {
-#     'name': input('Enter your name\n'),
-#     'age': int(input('Enter your age\n')),
-#     'location': {
-#         'country': input('Enter your country\n'),
-#         'city': input('Enter your city\n'),
-#         'street': input('Enter your street\n'),
-#         'number': int(input('Enter your number\n'))
-#     }
-# }, {
-#     'name': input('Enter your name\n'),
-#     'age': int(input('Enter your age\n')),
-#     'location': {
-#         'country': input('Enter your country\n'),
-#         'city': input('Enter your city\n'),
-#         'street': input('Enter your street\n'),
-#         'number': int(input('Enter your number\n')),
-#     }
-# }
-#
+p1, p2 = {
+    'name': input('Enter your name\n'),
+    'age': int(input('Enter your age\n')),
+    'location': {
+        'country': input('Enter your country\n'),
+        'city': input('Enter your city\n'),
+        'street': input('Enter your street\n'),
+        'number': int(input('Enter your number\n'))
+    }
+}, {
+    'name': input('Enter your name\n'),
+    'age': int(input('Enter your age\n')),
+    'location': {
+        'country': input('Enter your country\n'),
+        'city': input('Enter your city\n'),
+        'street': input('Enter your street\n'),
+        'number': int(input('Enter your number\n')),
+    }
+}
+
+print(p1, p2)
+
 p1_file = 'p1_file.txt'
 p2_file = 'p2_file.txt'
-#
-# with open(p1_file, mode='w') as my_file:
-#
-#     details = f"""name {p1['name']}
-# age {p1['age']}
-# country {p1['location']['country']}
-# city {p1['location']['city']}
-# street {p1['location']['street']}
-# number {p1['location']['number']}
-# """
-#     my_file.write(details)
-#
-# with open(p2_file, mode='w') as my_file:
-#
-#     details = f"""name {p2['name']}
-# age {p2['age']}
-# country {p2['location']['country']}
-# city {p2['location']['city']}
-# street {p2['location']['street']}
-# number {p2['location']['number']}
-# """
-#     my_file.write(details)
-#
+
+with open(p1_file, mode='w') as my_file:
+
+    details = f"""name {p1['name']}
+age {p1['age']}
+country {p1['location']['country']}
+city {p1['location']['city']}
+street {p1['location']['street']}
+number {p1['location']['number']}
+"""
+    my_file.write(details)
+
+with open(p2_file, mode='w') as my_file:
+
+    details = f"""name {p2['name']}
+age {p2['age']}
+country {p2['location']['country']}
+city {p2['location']['city']}
+street {p2['location']['street']}
+number {p2['location']['number']}
+"""
+    my_file.write(details)
+
 
 p3 = {
     'name': input('Enter your name\n'),
@@ -60,18 +62,32 @@ p1, p2 = {}, {}
 with open(p1_file,mode='r') as my_file:
     content = my_file.readlines()
 
+    # ['name Avner\n', 'age 42\n' , '']
+
     name_list = content[0].splitlines()
+    # ['name Avner]
     name = name_list[0].split()
+    # ['name' , 'Avner']
     p1[f'{name[0]}'] = name[1]
+    # { 'name' : 'Avner }
 
+    # ['name Avner\n', 'age 42\n' , '']
     age_list = content[1].splitlines()
+    # ['age 42']
     age = age_list[0].split()
-    p1[f'{age[0]}'] = age[1]
+    # ['age','42']
+    p1[f'{age[0]}'] = int(age[1])
+    # {'age' : 42}
 
+    # ['name Avner\n', 'age 42\n' , 'country israel\n']
     country_list = content[2].splitlines()
+    # ['country israel']
     country = country_list[0].split()
+    # ['country','israel']
     p1['location'] = {}
+    # {}
     p1['location'][f'{country[0]}'] = country[1]
+    # location {'country' : 'israel'}
 
     city_list = content[3].splitlines()
     city = city_list[0].split()
@@ -83,7 +99,7 @@ with open(p1_file,mode='r') as my_file:
 
     number_list = content[5].splitlines()
     number = number_list[0].split()
-    p1['location'][f'{number[0]}'] = number[1]
+    p1['location'][f'{number[0]}'] = int(number[1])
 
 with open(p2_file, mode='r') as my_file:
     content = my_file.readlines()
@@ -94,7 +110,7 @@ with open(p2_file, mode='r') as my_file:
 
     age_list = content[1].splitlines()
     age = age_list[0].split()
-    p2[f'{age[0]}'] = age[1]
+    p2[f'{age[0]}'] = int(age[1])
 
     country_list = content[2].splitlines()
     country = country_list[0].split()
@@ -111,7 +127,7 @@ with open(p2_file, mode='r') as my_file:
 
     number_list = content[5].splitlines()
     number = number_list[0].split()
-    p2['location'][f'{number[0]}'] = number[1]
+    p2['location'][f'{number[0]}'] = int(number[1])
 
 
 p3['friends'] = [p1, p2]
