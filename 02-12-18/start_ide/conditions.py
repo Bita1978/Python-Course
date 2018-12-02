9bc9b160711f41a024f9f999f96d4aebe392db61
EnoshTsur <enoshh12@gmail.com>
2018-12-02 22:41:12 +0200

    added conditions

--- 02-12-18/start_ide/conditions.py
+++ 02-12-18/start_ide/conditions.py
@@ -7,26 +7,26 @@
 <=
 """
 
-# print("1) 's' == 's' {}".format('s' == 's'))
-# print("2) 's' == 'a' {}".format('s' == 'a'))
-# print("3) 's' != 'a' {}".format('s' != 'a'))
-# print("4) 's' != 's' {}".format('s' != 's'))
-# print("5) '1' == 1 {}".format('1' == 1))
-# print("6) 1 == 1.0 {}".format(1 == 1.0))
-# print("6) not 1 == 1 {}".format(not 1 == 1))
+print("1) 's' == 's' {}".format('s' == 's'))
+print("2) 's' == 'a' {}".format('s' == 'a'))
+print("3) 's' != 'a' {}".format('s' != 'a'))
+print("4) 's' != 's' {}".format('s' != 's'))
+print("5) '1' == 1 {}".format('1' == 1))
+print("6) 1 == 1.0 {}".format(1 == 1.0))
+print("6) not 1 == 1 {}".format(not 1 == 1))
 
-# print("1) 1 < 3 {}".format(1 < 3))
-# print("2) 1 <= 1 {}".format(1 <= 1))
-# print("3) 5 > -1 {}".format(5 > 1))
-# print("4) 5 >= 5 {}".format(5 >= 5))
-# print("5) 5 < 6 > 7 {}".format(5 < 6 > 7))
-# print("6) 5 < 6 != 7 {}".format(5 < 6 != 7))
+print("1) 1 < 3 {}".format(1 < 3))
+print("2) 1 <= 1 {}".format(1 <= 1))
+print("3) 5 > -1 {}".format(5 > 1))
+print("4) 5 >= 5 {}".format(5 >= 5))
+print("5) 5 < 6 > 7 {}".format(5 < 6 > 7))
+print("6) 5 < 6 != 7 {}".format(5 < 6 != 7))
 
-# a, b, c = int(input('a')) , int(input('b')) , int(input('c'))
-# a = 3 , b = 2, c = 9
-# print('not (a > b != c) {}'.format(not (a > b != c)))
+a, b, c = int(input('a')) , int(input('b')) , int(input('c'))
 
-print("1) 3 > 2 and 1 == 1 {} ".format(3 > 1 and 1 == 2))
+print('not (a > b != c) {}'.format(not (a > b != c)))
+
+print("1) 3 > 2 and 1 == 1 {} ".format(3 > 1 and 1 == 1))
 print("2) 3 > 2 or 1 != 1 {}".format(3 > 2 or 1 != 1))
 print("3) 3 > 5 or 1 != 1 {}".format(3 > 5 or 1 != 1))
 print("4) (3 > 5 or 1 == 1) and (1 < 4 or 4 != 5) {}".format((3 > 5 or 1 == 1) and (1 < 4 or 4 != 5)))
