62394c050e1b5a3e2b3e41a979ccc42961ca2f0e
EnoshTsur <enoshh12@gmail.com>
2018-12-02 19:44:32 +0200

    'initial'

---
+++ 02-12-18/last-home-work/with_json.py
@@ -0,0 +1,55 @@
+import json
+
+# p1, p2 = {
+#     'name': input('Enter your name\n'),
+#     'age': int(input('Enter your age\n')),
+#     'location': {
+#         'country': input('Enter your country\n'),
+#         'city': input('Enter your city\n'),
+#         'street': input('Enter your street\n'),
+#         'number': int(input('Enter your number\n'))
+#     }
+# }, {
+#     'name': input('Enter your name\n'),
+#     'age': int(input('Enter your age\n')),
+#     'location': {
+#         'country': input('Enter your country\n'),
+#         'city': input('Enter your city\n'),
+#         'street': input('Enter your street\n'),
+#         'number': int(input('Enter your number\n')),
+#     }
+# }
+
+p1_file = 'p1_json_file.txt'
+p2_file = 'p2_json_file.txt'
+
+# with open(p1_file, mode='w') as my_file:
+#     my_file.write(json.dumps(p1))
+#
+# with open(p2_file, mode='w') as my_file:
+#     my_file.write(json.dumps(p2))
+
+p3 = {
+    'name': input('Enter your name\n'),
+    'age': int(input('Enter your age\n')),
+    'location': {
+        'country': input('Enter your country\n'),
+        'city': input('Enter your city\n'),
+        'street': input('Enter your street\n'),
+        'number': int(input('Enter your number\n'))
+    }
+}
+
+p1, p2 = {}, {}
+
+with open(p1_file, mode='r') as my_file:
+    p1 = json.loads(my_file.read())
+
+
+with open(p2_file, mode='r') as my_file:
+    p2 = json.loads(my_file.read())
+
+
+p3['friends'] = [p1, p2]
+
+print(p3)
---
+++ 02-12-18/start_ide/ex1.py
@@ -0,0 +1,10 @@
+with open('hi.txt', mode='w') as my_file:
+    my_file.write('hi there')
+
+with open('hi.txt', mode='r') as my_file:
+    print(my_file.read())
+
+
+
+
+
